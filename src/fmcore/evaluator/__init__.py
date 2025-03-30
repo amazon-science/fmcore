3238a091a6e6830690dcb5db0a38ebdfeb1824f5
Siba Rajendran <rajsiba@amazon.com>
2025-03-30 14:07:15 +0530

    Adding comments

--- src/fmcore/evaluator/__init__.py
+++ src/fmcore/evaluator/__init__.py
@@ -1,1 +1,3 @@
+# Importing evaluators to ensure they are registered with the Evaluator Registry
+from fmcore.evaluator.base_evaluator import BaseEvaluator
 from fmcore.evaluator.boolean_llm_judge_evaluator import BooleanLLMJudgeEvaluator
