ff3817e95267b81d74668e8c0284692488417a47
Siba Rajendran <rajsiba@amazon.com>
2025-03-30 14:05:23 +0530

    Adding comments

--- src/fmcore/evaluator/base_evaluator.py
+++ src/fmcore/evaluator/base_evaluator.py
@@ -28,40 +28,14 @@ class BaseEvaluator(Generic[I, O], MutableTyped, Registry, ABC):
     def _get_constructor_parameters(cls, *, evaluator_config: EvaluatorConfig) -> dict:
         """
         Generate the constructor parameters required for initializing a subclass.
-
-        This method is intended to be implemented by each subclass to provide the necessary parameters dynamically
-        based on the given `llm_config`.The purpose of this abstraction is to ensure that the registry pattern used
-        for creating subclasses does not require modification when new subclasses are introduced.
-
-        Each subclass has a different set of constructor parameters, and this method allows them to generate those
-        parameters as needed, ensuring flexibility and maintainability in the codebase.
-
-        ---
-
-        **How This Aligns with the Open/Closed Principle (OCP)**
-        The **Open/Closed Principle** states that a system should be **open for extension but closed for modification**.
-         This method enforces OCP by allowing new subclasses to be introduced **without modifying the base class** or
-         the registry/factory mechanism. Instead of altering existing code when a new subclass is introduced, the new
-         subclass simply implements `get_constructor_paramters`,ensuring it provides the appropriate constructor arguments.
-
-        **Closed for Modification**:
-        - The registry/factory does not need to be modified when adding a new subclass.
-        - The base class remains unchanged, preventing regressions in existing functionality.
-
-        **Open for Extension**:
-        - New LLM subclasses can be introduced freely, each defining how to extract its own constructor parameters.
-        - Different LLM implementations can have varying configurations, yet still be instantiated seamlessly within the existing system.
-
-        ---
-
+        To understand more checkout - BaseLLM in fmcore
+        https://github.com/amazon-science/fmcore/blob/labs-release/src/fmcore/llm/base_llm.py#L28
         Args:
             evaluator_config (EvaluatorConfig): The configuration object containing Evaluator-related settings.
 
         Returns:
             dict: A dictionary of keyword arguments (`**kwargs`) that can be used to instantiate the subclass dynamically.
 
-        By using this approach, we ensure that `BaseLLM` remains **stable and unmodified**, while allowing new
-        subclasses to introduce their own custom constructor parameters without breaking existing functionality.
         """
         pass
 
